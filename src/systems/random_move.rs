4444932966c5e90b65d3702a205fb2338003ba5a
Michal Lonski <michal.lonski@du.co>
2021-12-31 18:02:09 +0100

    added combat system

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -3,22 +3,53 @@ use crate::prelude::*;
 #[system]
 #[read_component(MovingRandomly)]
 #[read_component(Point)]
+#[read_component(Player)]
+#[read_component(Health)]
 pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut movers = <(Entity, &Point, &MovingRandomly)>::query();
+    let mut positions = <(Entity, &Point, &Health)>::query();
     let mut rng = RandomNumberGenerator::new();
-    movers.iter_mut(ecs).for_each(|(entity, pos, _)| {
+    movers.iter(ecs).for_each(|(entity, pos, _)| {
+        // calculate random destination
         let destination = match rng.range(0, 4) {
             0 => Point::new(-1, 0),
             1 => Point::new(1, 0),
             2 => Point::new(0, -1),
             _ => Point::new(0, 1),
         } + *pos;
-        commands.push((
-            (),
-            WantsToMove {
-                entity: *entity,
-                destination,
-            },
-        ));
+
+        // try attack
+        let mut attacked = false;
+        positions
+            .iter(ecs)
+            .filter(|(_, target_pos, _)| **target_pos == destination)
+            .for_each(|(victim, _, _)| {
+                if ecs
+                    .entry_ref(*victim)
+                    .unwrap()
+                    .get_component::<Player>()
+                    .is_ok()
+                {
+                    commands.push((
+                        (),
+                        WantsToAttack {
+                            attacker: *entity,
+                            victim: *victim,
+                        },
+                    ));
+                }
+                attacked = true;
+            });
+
+        //move if not attacked
+        if !attacked {
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+        }
     });
 }
