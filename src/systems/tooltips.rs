8adc3cac309ff613bbe10397606230ea48d1dc37
Michal Lonski <michal.lonski@du.co>
2022-01-03 22:40:25 +0100

    added field of view

--- src/systems/tooltips.rs
+++ src/systems/tooltips.rs
@@ -4,7 +4,10 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Name)]
 #[read_component(Health)]
+#[read_component(FieldOfView)]
 pub fn tooltips(ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camera: &Camera) {
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+    let player_fov = fov.iter(ecs).next().unwrap();
     let mut positions = <(Entity, &Point, &Name)>::query();
     let offset = Point::new(camera.left_x, camera.top_y);
     let map_pos = *mouse_pos + offset;
@@ -13,6 +16,7 @@ pub fn tooltips(ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camer
     positions
         .iter(ecs)
         .filter(|(_, pos, _)| **pos == map_pos)
+        .filter(|(_, pos, _)| player_fov.visible_tiles.contains(*pos))
         .for_each(|(entity, _, name)| {
             let screen_pos = *mouse_pos * 4;
             let display =
